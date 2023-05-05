0530abbf1382f01219bbe6f03ba76068e352879c
felipebuitrago <49107226+felipebuitrago@users.noreply.github.com>
2023-05-05 18:50:59 -0500

    almost finished

--- app/controllers/inventarioCtrl.js
+++ app/controllers/inventarioCtrl.js
@@ -1,34 +1,33 @@
 const { response } = require("express");
 const mongoose = require("mongoose");
 const  generarJWT  = require("../helpers/jwt");
-const Inventario = require('../models/Inventario')
+const Inventario = require('../models/Inventario');
+const Producto = require('../models/Producto');
 
 //CREAR UN NUEVO movimiento de Inventario
 const crearMovimiento = async(req, res = response) => {
 
     const token = await generarJWT(req.uid, req.name);
 
-    let movimiento = {
+    const movimiento = new Inventario({
         registrado_por: req.name,
-        tipo_transaccion: "req.body.tipodetransaccion",
-        producto: "req.body.producto",
-        presentacion: "req.body.talla",
-        almacen: "req.body.almacen",
-        paciente_proveedor: "req.body.paciente",
+        tipo_transaccion: req.body.tipoMovimiento,
+        producto: req.body.producto,
+        presentacion: req.body.presentacion,
+        almacen: req.body.almacen,
+        paciente_proveedor: req.body.tercero,
         factura: "req.body.factura",
-        fecha: new Date(),
-        cantidad: 15,
-        factura: "req.body.factura456",
-        nota: "req.body.nota"
-    }
+        fecha: req.body.fecha,
+        cantidad: req.body.cantidad,
+        nota: req.body.nota
+    });
+    await movimiento.save();
 
-    const result = new Inventario(movimiento);
-    await result.save();
+    await Producto.findByIdAndUpdate(req.body.idProducto, {stock : req.body.nuevoStock});
 
     return res.status(201).json({
         msg:"$$$$$$$ U are going through crearMovimiento $$$$$$$",
-        newToken: token,
-        result 
+        newToken: token
     })
 }
 
