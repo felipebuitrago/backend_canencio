2384d58426d60ab211fa545f544fd6c811ddd518
felipebuitrago <49107226+felipebuitrago@users.noreply.github.com>
2023-05-06 03:18:37 -0500

    problema trasladando si estaba en + de 2 almacenes, solucionado

--- app/controllers/productosCtrl.js
+++ app/controllers/productosCtrl.js
@@ -189,71 +189,46 @@ const trasladarProducto = async(req, res = response) => {
   await movimientoEgreso.save();
   await movimientoIngreso.save();
 
-  //buscar productos con el mismo nombre y talla 
-  let existe = await Producto.aggregate(
+  let existe2 = await Producto.aggregate(
     [
       {
-        $lookup: {
-          from: "almacenes",
-          localField: "almacen",
-          foreignField: "_id",
-          as: "almacen"
-        }
-      },
-      {
         $match:{
-          nombre: nombreProductoOrigen,
-          presentacion: presentacion
+          almacen: { '$eq': new mongoose.Types.ObjectId(almacenDestino) },
+          presentacion : presentacion,
+          nombre: nombreProductoOrigen
         }
       }
-    ])
+  ]);
   
-  if (existe.length === 2) {
-    existe.map(async(current,index) => {
-      
-      if(current.almacen[0]._id.equals(almacenDestino)){
+  if(existe2.length === 1){
+    const newStockDestino = parseInt(existe2[0].stock) + parseInt(cantidad); 
+    await Producto.findByIdAndUpdate(existe2[0]._id.toString(),{stock:newStockDestino});
   
-        const newStockDestino = parseInt(current.stock) + parseInt(cantidad); 
-        await Producto.findByIdAndUpdate(current._id.toString(),{stock:newStockDestino});
-  
-        const newStockOrigen = parseInt(stockActualOrigen) - parseInt(cantidad); 
-        await Producto.findByIdAndUpdate(idProductoOrigen,{stock:newStockOrigen});
-
-        let result2 = await Producto.aggregate(aggregations)
-
-        return res.status(201).json({
-            msg:"$$$$$$$ U are going through trasladarProducto $$$$$$$",
-            newToken: token,
-            result:result2
-        })
-      }
-    })
-  }
-  else if(existe.length === 1){
-    let producto = {
-      stock: parseInt(cantidad),
-      nombre: nombreProductoOrigen,
-      presentacion: presentacion,
-      proveedor: proveedorID,
-      categoria:  categoriasIDs,
-      almacen: almacenDestino,
-    }
-    const productoDB = new Producto(producto);
-    await productoDB.save();
-  
-    //actualiza stock del producto donde se toman las existencias
     const newStockOrigen = parseInt(stockActualOrigen) - parseInt(cantidad); 
-    await Producto.findByIdAndUpdate(idProductoOrigen,{stock:newStockOrigen});
-    
-    let result2 = await Producto.aggregate(aggregations)
-  
-    return res.status(201).json({
-        msg:"$$$$$$$ U are going through trasladarProducto $$$$$$$",
-        newToken: token,
-        result:result2
-    })
-  }  
+    await Producto.findByIdAndUpdate(idProductoOrigen,{stock:newStockOrigen}); 
+  }
+  else if(existe2.length === 0){
 
+    let producto = {
+        stock: parseInt(cantidad),
+        nombre: nombreProductoOrigen,
+        presentacion: presentacion,
+        proveedor: proveedorID,
+        categoria:  categoriasIDs,
+        almacen: almacenDestino,
+      }
+      const productoDB = new Producto(producto);
+      await productoDB.save();
+      //actualiza stock del producto donde se toman las existencias
+      const newStockOrigen = parseInt(stockActualOrigen) - parseInt(cantidad); 
+      await Producto.findByIdAndUpdate(idProductoOrigen,{stock:newStockOrigen});
+  }
+  let result = await Producto.aggregate(aggregations);
+  return res.status(201).json({
+      msg:"$$$$$$$ U are going through trasladarProducto $$$$$$$",
+      newToken: token,
+      result
+  })
 }
 
 module.exports = {
